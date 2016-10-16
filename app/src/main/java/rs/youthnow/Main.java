001833c4dcfbd318e113dfeb47a1752646c44621
Stefan <stefke2105996@gmail.com>
2016-10-16 10:35:19 +0200

    added nav bar

--- app/src/main/java/rs/youthnow/Main.java
+++ app/src/main/java/rs/youthnow/Main.java
@@ -1,9 +1,6 @@
 package rs.youthnow;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -71,17 +68,25 @@ public class Main extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_studenti) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_dogadjaji) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_it) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_nauka) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_lifestyle) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_online_psiholog) {
+
+        } else if (id == R.id.nav_kulturiska) {
+
+        } else if (id == R.id.nav_zabava) {
+
+        } else if (id == R.id.nav_intervjui) {
+
+        } else if (id == R.id.nav_kontakt) {
 
         }
 
--- app/src/main/java/rs/youthnow/PostItemAdapter.java
+++ app/src/main/java/rs/youthnow/PostItemAdapter.java
@@ -2,12 +2,7 @@ package rs.youthnow;
 
 import android.app.Activity;
 import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 
 public class PostItemAdapter extends ArrayAdapter<PostData> {
